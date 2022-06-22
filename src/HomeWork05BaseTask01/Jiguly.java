b461f679be6bda210cefa7eef4e0c462f5d2c629
AStrochka <grandtilia@gmail.com>
2022-06-22 19:30:16 +0500

    Добавляем всё в первый репозиторий

---
+++ src/HomeWork05BaseTask01/Jiguly.java
@@ -0,0 +1,8 @@
+package HomeWork05BaseTask01;
+
+public class Jiguly extends Car{
+
+    public void broken() {
+        System.out.println("Жигули ломается");
+    }
+}
---
+++ src/HomeWork05BaseTask01/Toyota.java
@@ -0,0 +1,8 @@
+package HomeWork05BaseTask01;
+
+public class Toyota extends Car{
+
+    public void turnMusic() {
+        System.out.println("Тойота включает музыку");
+    }
+}
